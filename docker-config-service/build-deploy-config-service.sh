2ce5245eccc3d88e2f0b6198577325f714e209e6
Kai Saborowski <ksaborowski@MacBook-Pro-von-Kai.local>
2022-08-08 11:20:06 +0200

    Fix Docker settings.

--- docker-config-service/build-deploy-config-service.sh
+++ docker-config-service/build-deploy-config-service.sh
@@ -1,4 +1,4 @@
 docker stop docker-config-service
 docker rm docker-config-service
 docker build . -t docker-config-service:1.0.0
-docker run --name docker-config-service -p 8888:8888 -d docker.io/library/docker-config-service:1.0.0
+docker run --name docker-config-service -p 8888:8888 -d docker-config-service:1.0.0
--- docker-registry-service/build-deploy-service-registry.sh
+++ docker-registry-service/build-deploy-service-registry.sh
@@ -1,4 +1,4 @@
 docker stop docker-service-registry
 docker rm docker-service-registry
 docker build . -t docker-service-registry:1.0.0
-docker run --name docker-service-registry -p 8761:8761 -d docker.io/library/docker-service-registry:1.0.0
+docker run --name docker-service-registry -p 8761:8761 -d docker-service-registry:1.0.0
